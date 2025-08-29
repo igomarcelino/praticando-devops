d05c13fb3027499c7d994472dbfbebdd79c0908d
Igo Marcelino <geslucian@hotmail.com>
2025-08-29 13:26:44 -0300

    feat:criando repo

---
+++ src/main/java/org/igomarcelino/praticando_devops/PraticandoDevopsApplication.java
@@ -0,0 +1,13 @@
+package org.igomarcelino.praticando_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/igomarcelino/praticando_devops/PraticandoDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.igomarcelino.praticando_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
